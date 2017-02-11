c17b856c5e46e4b06926e5a712cb35299bbaca74
zhijazi <hijazizein2014@gmail.com>
2017-02-10 21:36:15 -0500

    Documented second function and close IO file

--- configparser.py
+++ configparser.py
@@ -6,9 +6,16 @@ def readConfigFile():
     Returns string being read.
     """
     config_file = open("config/effects.txt")
-    return config_file.read()
+    config_data = config_file.read()
+    config_file.close()
+    return config_data
 
 def parseConfigFile(config_data):
+    """
+    Parse the data from the file into respective
+    objects.
+    Currently prints the objects to the screen for testing.
+    """
     config_data = json.loads(config_data)
     print("Effect: " + config_data['effect'])
     parameters = config_data['parameters']
