7a1bdc768bcf0c5ca50643dcdc82a3feb7aff102
zhijazi <hijazizein2014@gmail.com>
2017-02-11 21:42:26 -0500

    Implemented Effects from Config Functionality
    1. The main python program now successfully adds the desired selected effect from the config file.
    (Does not currently account for parameters)
        a. Currently supports the loading of only one effect
    2. Added high level function in configparser.py to get all effects from the config file.
    3. Refactored configparser.py to conform to PEP 8 standards

--- configparser.py
+++ configparser.py
@@ -1,6 +1,6 @@
 import json
 
-def readConfigFile():
+def read_config_file():
     """
     Reads the configuration file stored in config/
     Returns string being read.
@@ -10,16 +10,15 @@ def readConfigFile():
     config_file.close()
     return config_data
 
-def parseConfigFile(config_data):
+def parse_config_file(config_data):
     """
     Parse the data from the file into respective
     objects.
     Currently prints the objects to the screen for testing.
     """
     config_data = json.loads(config_data)
-    print("Effect: " + config_data['effect'])
-    parameters = config_data['parameters']
-    print("Parameters: " + str(parameters))
+    return config_data
 
-contents = readConfigFile()
-parseConfigFile(contents)
+def get_effects():
+    file_contents = read_config_file()
+    return parse_config_file(file_contents)
--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import pyo
 import time
+import configparser
 
 # Start the server necessary for Pyo to get audio streams from JACK
 s = pyo.Server(audio='jack', nchnls=1).boot()
 s.start()
 
 # Read input from the audio device on channel 1
-audioIn = pyo.Input(chnl=0).out()
+audioIn = pyo.Input(chnl=0)
+
+effects_dict = configparser.get_effects()
+for effect in effects_dict.keys():
+    #print("Effect: " + effect + ", Params: " + str(effects_dict[effect]))
+    if effect == 'distortion':
+        #distortion stuff
+        print("Enable distortion effect")
+        distortion = pyo.Disto(audioIn, drive=0.75, slope=0.5, mul=1, add=0).out()
+    elif effect == 'delay':
+        #delay stuff
+        print("Enable delay effect")
+        delay = pyo.Delay(audioIn, delay=.25, feedback=0.5, mul=0.5).out()
+    elif effect == 'reverb':
+        #reverb stuff
+        print("Enable reverb effect")
+        # there's a TON of reverb effects in Pyo. Gotta find out which one is the right to implement.
+        reverb = pyo.Freeverb(audioIn, size=0.5, damp=0.5, bal=0.5, mul=1, add=0).out()
+    elif effect == 'chorus':
+        #chorus stuff
+        print("Enable chorus effect")
+        chorus = pyo.Chorus(audioIn, depth=1, feedback=0.25, bal=0.5, mul=1, add=0).out()
+    elif effect == 'freqshift':
+        #frequency shift stuff
+        print("Enable frequency shift effect")
+        freqshift = pyo.FreqShift(audioIn, shift=100, mul=1, add=0).out()
+    elif effect == 'harmonizer':
+        #harmonizer stuff
+        print("Enable harmonizer effect")
+        harmonizer = pyo.Harmonizer(audioIn, transpo=-7.0, feedback=0, winsize=0.1, mul=1, add=0).out()
+
+#audioIn.out()
 
 # Eliminate need for GUI with loop - eventually to be used for updating effects.
 while True:
