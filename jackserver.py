adc30bd519a95875afcaf950df4dcaf2fca5286d
zhijazi <hijazizein2014@gmail.com>
2017-02-18 13:04:17 -0500

    Beginnings of Jack Auto Start Functionality
    Functions to list the capture devices in a list created.
    Functions to list the playback devices in a list created.
    Function to start jack from a command line process using inputport and outputport created.
        Process runs in foreground -> needs to be changed to background process.

---
+++ jackserver.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def start_jack_server(inputport, outputport):
+    process = subprocess.call('jackd -P 70 -d alsa -r 48000 -p 1024 -n 2 -D -C ' + inputport + '-P ' + outputport, shell=True)
+
+def get_input_devices():
+    process = subprocess.Popen(['arecord', '-l'], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    return out
+
+def get_output_devices():
+    process = subprocess.Popen(['aplay', '-l'], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    return out
+
+def filter_shell_output(data):
+    sound_devices = []
+    data_arr = data.split("\n")
+    for line in data_arr:
+        if 'card' in line:
+            sound_devices.append(line)
+    return sound_devices
+
+start_jack_server(1, 1)
