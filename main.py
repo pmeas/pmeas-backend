da5214f827c1195e69ac72154b29aa3ae9d8e7e5
zhijazi <hijazizein2014@gmail.com>
2017-02-22 21:58:36 -0500

    Refactored parameters for arbitrary effect number
    Refactored the paramaters branch which was based off the
    old implementation of effects that is limited to two effects.

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pyo
 import time
 import configparser
 
-effect_one = None
-effect_two = None
-
 # Start the server necessary for Pyo to get audio streams from JACK
 s = pyo.Server(audio='jack', nchnls=1).boot()
 s.start()
 
+enabled_effects = []
+
 # Read input from the audio device on channel 1
-audio_in = pyo.Input(chnl=0)
+enabled_effects.append(pyo.Input(chnl=0))
 
 effects_dict = configparser.get_effects()
 for effect in effects_dict.keys():
@@ -19,54 +18,31 @@ for effect in effects_dict.keys():
     if effect == 'distortion':
         #distortion stuff
         print("Enable distortion effect")
-        if effect_one is None:
-            effect_one = pyo.Disto(audio_in, drive=int(params['drive']), slope=int(params['slope']), mul=1, add=0).out()
-        else:
-            effect_two = pyo.Disto(effect_one, drive=int(params['drive']), slope=int(params['slope']), mul=1, add=0).out()
+        enabled_effects.append(pyo.Disto(enabled_effects[len(enabled_effects)-1], drive=int(params['drive']), slope=int(params['slope']), mul=1, add=0))
     elif effect == 'delay':
         #delay stuff
         print("Enable delay effect")
-        if effect_one is None:
-            effect_one = pyo.Delay(audio_in, delay=int(params['delay']), feedback=int(params['feedback']), mul=0.5).out()
-        else:
-            effect_two = pyo.Delay(effect_one, delay=int(params['delay']), feedback=int(params['feedback']), mul=0.5).out()
+        enabled_effects.append(pyo.Delay(enabled_effects[len(enabled_effects)-1], delay=int(params['delay']), feedback=int(params['feedback']), mul=0.5))
     elif effect == 'reverb':
         #reverb stuff
         print("Enable reverb effect")
         # there's a TON of reverb effects in Pyo. Gotta find out which one is the right to implement.
-        if effect_one is None:
-            effect_one = pyo.STRev(audio_in, inpos=0.25, revtime=int(params['revtime']), cutoff=int(params['cutoff']), bal=int(params['balance']), roomSize=int(params['roomsize'])).out()
-        else:
-            effect_two = pyo.STRev(effect_one, inpos=0.25, revtime=int(params['revtime']), cutoff=int(params['cutoff']), bal=int(params['balance']), roomSize=int(params['roomsize'])).out()
+        enabled_effects.append(pyo.STRev(enabled_effects[len(enabled_effects)-1], inpos=0.25, revtime=int(params['revtime']), cutoff=int(params['cutoff']), bal=int(params['balance']), roomSize=int(params['roomsize'])))
     elif effect == 'chorus':
         #chorus stuff
         print("Enable chorus effect")
-        if effect_one is None:
-            effect_one = pyo.Chorus(audio_in, depth=int(params['depth']), feedback=int(params['feedback']), bal=int(params['balance'], mul=1, add=0).out()
-        else:
-            effect_two = pyo.Chorus(effect_one, depth=int(params['depth']), feedback=int(params['feedback']), bal=int(params['balance'], mul=1, add=0).out()
+        enabled_effects.append(pyo.Chorus(enabled_effects[len(enabled_effects)-1], depth=int(params['depth']), feedback=int(params['feedback']), bal=int(params['balance']), mul=1, add=0))
     elif effect == 'freqshift':
         #frequency shift stuff
         print("Enable frequency shift effect")
-        if effect_one is None:
-            effect_one = pyo.FreqShift(audio_in, shift=int(params['shift']), mul=1, add=0).out()
-        else:
-            effect_two = pyo.FreqShift(effect_one, shift=int(params['shift']), mul=1, add=0).out()
+        enabled_effects.append(pyo.FreqShift(enabled_effects[len(enabled_effects)-1], shift=int(params['shift']), mul=1, add=0))
     elif effect == 'harmonizer':
         #harmonizer stuff
         print("Enable harmonizer effect")
-        if effect_one is None:
-            effect_one = pyo.Harmonizer(audio_in, transpo=int(params['transpose']), feedback=int(params['feedback']), winsize=0.1, mul=1, add=0).out()
-        else:
-            effect_two = pyo.Harmonizer(effect_one, transpo=int(params['transpose']), feedback=int(params['feedback']), winsize=0.1, mul=1, add=0).out()
-    else:
-        # For invalid input just output direct audio
-        if effect_one is None:
-            audio_in.out()
+        enabled_effects.append(pyo.Harmonizer(enabled_effects[len(enabled_effects)-1], transpo=int(params['transpose']), feedback=int(params['feedback']), winsize=0.1, mul=1, add=0))
 
+enabled_effects[len(enabled_effects)-1].out()
 
 # Eliminate need for GUI with loop - eventually to be used for updating effects.
 while True:
     time.sleep(1)
-
-
