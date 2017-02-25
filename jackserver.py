4ee144493b9c7f35dfc4c0a0c07daf1d2697376e
zhijazi <hijazizein2014@gmail.com>
2017-02-25 17:31:06 -0500

    Beginnings of proper pep8 documentation

--- jackserver.py
+++ jackserver.py
@@ -1,9 +1,19 @@
 import subprocess
 
 def start_jack_server(inputport, outputport):
+    """
+    Start the JACK server.
+
+    Keyword arguments:
+    inputport -- the port on which to listen for audio data
+    outputport -- the port on which to send audio data to.
+
+    Called ONLY when no existing JACK server is running on the machine.
+    """
     process = subprocess.call('jackd -P 70 -d alsa -r 48000 -p 1024 -n 2 -D -C &' + inputport + '-P ' + outputport, shell=True)
 
 def get_input_devices():
+    """List the output devices captured by the ALSA interface"""
     process = subprocess.Popen(['arecord', '-l'], stdout=subprocess.PIPE)
     out, err = process.communicate()
     return out
